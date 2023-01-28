fc4b9e5ed85ddde6201b610f6f1d1c927d621768
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-01-29 07:55:10 +1300

    File: fish config `config.fish`

---
+++ config.fish
@@ -0,0 +1,10 @@
+pfetch
+
+# Time Table
+echo "====================================="
+~/./Documents/Code\ Files/C\#/TimeTable\ Right\ Now/bin/Debug/net7.0/TimeTable\ Right\ Now
+echo "====================================="
+
+# Alias'
+alias codedir="cd ~/Documents/Code\ Files/"
+alias tt="~/./Documents/Code\ Files/C\#/TimeTable\ Right\ Now/bin/Debug/net7.0/TimeTable\ Right\ Now"
